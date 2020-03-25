fb770cf733bd69ec8fa94996e6e2f5c94e656784
swethusiast <aitc.dev@gmail.com>
2020-03-25 16:52:19 +0200

    chart

--- src/Components/new-patient/HemoDynamic/index.js
+++ src/Components/new-patient/HemoDynamic/index.js
@@ -7,244 +7,246 @@ import { HemoDynamicIcon } from '../../shared/Icons';
 import './styles.scss';
 
 class BloodPressure extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			edit: false,
-			systolic_actions: [],
-			systolic_saved_actions: [],
-			diastolic_actions: [],
-			diastolic_initial_actions: []
-		};
-	}
-	componentDidMount() {
-		//Systolic intial values
-		const systolic_initial_actions = [
-			{
-				id: 1,
-				in_value: 120
-			},
-			{
-				id: 2,
-				in_value: 140
-			},
-			{
-				id: 3,
-				in_value: 160
-			},
-			{
-				id: 4,
-				in_value: 200
-			}
-		];
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit: false,
+            systolic_actions: [],
+            systolic_saved_actions: [],
+            diastolic_actions: [],
+            diastolic_initial_actions: [],
+        };
+    }
+    componentDidMount() {
+        //Systolic intial values
+        const systolic_initial_actions = [
+            {
+                id: 1,
+                in_value: 120,
+            },
+            {
+                id: 2,
+                in_value: 140,
+            },
+            {
+                id: 3,
+                in_value: 160,
+            },
+            {
+                id: 4,
+                in_value: 200,
+            },
+        ];
 
-		// Diastolic intial values
-		const diastolic_initial_actions = [
-			{
-				id: 5,
-				in_value: 75
-			},
-			{
-				id: 6,
-				in_value: 80
-			},
-			{
-				id: 7,
-				in_value: 90
-			},
-			{
-				id: 8,
-				in_value: 100
-			}
-		];
+        // Diastolic intial values
+        const diastolic_initial_actions = [
+            {
+                id: 5,
+                in_value: 75,
+            },
+            {
+                id: 6,
+                in_value: 80,
+            },
+            {
+                id: 7,
+                in_value: 90,
+            },
+            {
+                id: 8,
+                in_value: 100,
+            },
+        ];
 
-		this.setState({
-			//	systolic_actions
-			systolic_actions: JSON.parse(JSON.stringify(systolic_initial_actions)),
-			systolic_saved_actions: JSON.parse(JSON.stringify(systolic_initial_actions)),
-			//
-			diastolic_actions: JSON.parse(JSON.stringify(diastolic_initial_actions)),
-			diastolic_saved_actions: JSON.parse(JSON.stringify(diastolic_initial_actions))
-		});
-		// console.log(this.state.systolic_initial_actions);
-	}
-	updateActionData = (ActionID, name, actionData) => {
-		var systolic = [ ...this.state.systolic_actions ];
-		var diastolic = [ ...this.state.diastolic_actions ];
+        this.setState({
+            //	systolic_actions
+            systolic_actions: JSON.parse(JSON.stringify(systolic_initial_actions)),
+            systolic_saved_actions: JSON.parse(JSON.stringify(systolic_initial_actions)),
+            //
+            diastolic_actions: JSON.parse(JSON.stringify(diastolic_initial_actions)),
+            diastolic_saved_actions: JSON.parse(JSON.stringify(diastolic_initial_actions)),
+        });
+        // console.log(this.state.systolic_initial_actions);
+    }
+    updateActionData = (ActionID, name, actionData) => {
+        var systolic = [ ...this.state.systolic_actions ];
+        var diastolic = [ ...this.state.diastolic_actions ];
 
-		systolic = systolic.map((data) => {
-			if (data.id == ActionID) {
-				data[name] = actionData[name];
-			}
-			return data;
-		});
-		diastolic = diastolic.map((data) => {
-			if (data.id == ActionID) {
-				data[name] = actionData[name];
-			}
-			return data;
-		});
-		// console.log('update', systolic, diastolic);
-		this.setState({
-			systolic_actions: systolic,
-			diastolic_actions: diastolic
-		});
-	};
-	renderHeaderButtons = () => {
-		const {
-			edit,
-			systolic_actions,
-			systolic_saved_actions,
-			diastolic_actions,
-			diastolic_saved_actions
-		} = this.state;
-		if (edit) {
-			return (
-				<React.Fragment>
-					<button
-						className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall mx-1"
-						onClick={() => {
-							this.setState({
-								edit: false,
-								systolic_saved_actions: JSON.parse(JSON.stringify(systolic_actions)),
-								diastolic_saved_actions: JSON.parse(JSON.stringify(diastolic_actions))
-							});
-						}}
-					>
-						<SaveIcon /> Save
-					</button>
-					<button
-						className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall mx-1"
-						onClick={() => {
-							this.setState({
-								edit: false,
-								systolic_actions: JSON.parse(JSON.stringify(systolic_saved_actions)),
+        systolic = systolic.map((data) => {
+            if (data.id == ActionID) {
+                data[name] = actionData[name];
+            }
+            return data;
+        });
+        diastolic = diastolic.map((data) => {
+            if (data.id == ActionID) {
+                data[name] = actionData[name];
+            }
+            return data;
+        });
+        // console.log('update', systolic, diastolic);
+        this.setState({
+            systolic_actions: systolic,
+            diastolic_actions: diastolic,
+        });
+    };
+    renderHeaderButtons = () => {
+        const {
+            edit,
+            systolic_actions,
+            systolic_saved_actions,
+            diastolic_actions,
+            diastolic_saved_actions,
+        } = this.state;
+        if (edit) {
+            return (
+                <React.Fragment>
+                    <button
+                        className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall mx-1"
+                        onClick={() => {
+                            this.setState({
+                                edit: false,
+                                systolic_saved_actions: JSON.parse(JSON.stringify(systolic_actions)),
+                                diastolic_saved_actions: JSON.parse(JSON.stringify(diastolic_actions)),
+                            });
+                        }}
+                    >
+                        <SaveIcon /> Save
+                    </button>
+                    <button
+                        className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall mx-1"
+                        onClick={() => {
+                            this.setState({
+                                edit: false,
+                                systolic_actions: JSON.parse(JSON.stringify(systolic_saved_actions)),
 
-								diastolic_actions: JSON.parse(JSON.stringify(diastolic_saved_actions))
-							});
-						}}
-					>
-						<CancelIcon /> Cancel
-					</button>
-				</React.Fragment>
-			);
-		} else {
-			return (
-				<React.Fragment>
-					<button
-						className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall"
-						onClick={() => {
-							this.setState({ edit: true });
-						}}
-					>
-						<EditIcon /> Edit
-					</button>
-				</React.Fragment>
-			);
-		}
-	};
-	//1
-	renderActionCells = () => {
-		return (
-			<React.Fragment>
-				{this.state.systolic_actions.map((value, key) => (
-					<React.Fragment key={key}>
-						<td>
-							<ActionCell
-								disabled={!this.state.edit}
-								data={value}
-								index={key}
-								updateActionData={this.updateActionData}
-							/>
-						</td>
-					</React.Fragment>
-				))}
-			</React.Fragment>
-		);
-	};
-	//diastolic
+                                diastolic_actions: JSON.parse(JSON.stringify(diastolic_saved_actions)),
+                            });
+                        }}
+                    >
+                        <CancelIcon /> Cancel
+                    </button>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <button
+                        className="btn btn-transparent py-0 pl-0 pr-3 text-white weight-600 font-Lsmall"
+                        onClick={() => {
+                            this.setState({ edit: true });
+                        }}
+                    >
+                        <EditIcon /> Edit
+                    </button>
+                </React.Fragment>
+            );
+        }
+    };
+    //1
+    renderActionCells = () => {
+        return (
+            <React.Fragment>
+                {this.state.systolic_actions.map((value, key) => (
+                    <React.Fragment key={key}>
+                        <td>
+                            <ActionCell
+                                disabled={!this.state.edit}
+                                data={value}
+                                index={key}
+                                updateActionData={this.updateActionData}
+                            />
+                        </td>
+                    </React.Fragment>
+                ))}
+            </React.Fragment>
+        );
+    };
+    //diastolic
 
-	renderDiastolicActionCells = () => {
-		return (
-			<React.Fragment>
-				{this.state.diastolic_actions.map((value, key) => (
-					<React.Fragment key={key}>
-						<td>
-							<ActionCell
-								disabled={!this.state.edit}
-								data={value}
-								index={key}
-								updateActionData={this.updateActionData}
-							/>
-						</td>
-					</React.Fragment>
-				))}
-			</React.Fragment>
-		);
-	};
+    renderDiastolicActionCells = () => {
+        return (
+            <React.Fragment>
+                {this.state.diastolic_actions.map((value, key) => (
+                    <React.Fragment key={key}>
+                        <td>
+                            <ActionCell
+                                disabled={!this.state.edit}
+                                data={value}
+                                index={key}
+                                updateActionData={this.updateActionData}
+                            />
+                        </td>
+                    </React.Fragment>
+                ))}
+            </React.Fragment>
+        );
+    };
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="cardiac-chart">
-					<div className="tab_card">
-						<div className="card_heading">
-							<div className="card_heading_left_side">
-								<HemoDynamicIcon />
-								<h3> Hemodynamic - Blood Pressure </h3>
-							</div>
-							<span>{this.renderHeaderButtons()}</span>
-						</div>
+    render() {
+        return (
+            <React.Fragment>
+                <div className="cardiac-chart">
+                    <div className="tab_card">
+                        <div className="card_heading">
+                            <div className="card_heading_left_side">
+                                <HemoDynamicIcon />
+                                <h3> Hemodynamic - Blood Pressure </h3>
+                            </div>
+                            <span>{this.renderHeaderButtons()}</span>
+                        </div>
 
-						<div className="card_fields">
-							<Row>
-								<Col md={4} className="cardiac-left-side">
-									<div>
-										<h4>a. Measured every 2 hours</h4>
-									</div>
-								</Col>
-								<Col md={8} className="cardiac-right-side">
-									<h4>b. The predefined readings:</h4>
-									<table className="table">
-										<thead>
-											<tr>
-												<th>Critical (below)</th>
-												<th>Low (below)</th>
-												<th>High (Above)</th>
-												<th>Critical (High)</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>{this.renderActionCells()}</tr>
-										</tbody>
-									</table>
-									<table className="table">
-										<thead>
-											<tr>
-												<th>Critical (below)</th>
-												<th>Low (below)</th>
-												<th>High (Above)</th>
-												<th>Critical (High)</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>{this.renderDiastolicActionCells()}</tr>
-										</tbody>
-									</table>
-									<div className="hemoDynamiChart">
-										<CardiacChart
-											diastolic_chart_data={this.state.diastolic_saved_actions}
-											systolic_chart_data={this.state.systolic_saved_actions}
-										/>
-									</div>
-								</Col>
-							</Row>
-						</div>
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+                        <div className="card_fields">
+                            <Row>
+                                <Col md={4} className="cardiac-left-side">
+                                    <div>
+                                        <h4>a. Measured every 2 hours</h4>
+                                    </div>
+                                </Col>
+                                <Col md={8} className="cardiac-right-side">
+                                    <h4>b. The predefined readings:</h4>
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Critical (below)</th>
+                                                <th>Low (below)</th>
+                                                <th>High (Above)</th>
+                                                <th>Critical (High)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            systolic_chart_data
+                                            <tr>{this.renderActionCells()}</tr>
+                                        </tbody>
+                                    </table>
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Critical (below)</th>
+                                                <th>Low (below)</th>
+                                                <th>High (Above)</th>
+                                                <th>Critical (High)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            diastolic_chart_data
+                                            <tr>{this.renderDiastolicActionCells()}</tr>
+                                        </tbody>
+                                    </table>
+                                    <div className="hemoDynamiChart">
+                                        <CardiacChart
+                                            diastolic_chart_data={this.state.diastolic_saved_actions}
+                                            systolic_chart_data={this.state.systolic_saved_actions}
+                                        />
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default BloodPressure;
